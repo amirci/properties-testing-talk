f815adb70ed6461122a136147767d4bc53597d28
Amir Barylko <amir@barylko.com>
2017-10-05 13:17:34 -0700

    Added test for finding basement

--- src/count_floors/core.clj
+++ src/count_floors/core.clj
@@ -1,5 +1,28 @@
 (ns count-floors.core)
 
+(defn walk-one
+  [f i]
+  (+ f (if (= i \() 1 -1)))
+
 (defn count-total
   [s]
-  (reduce #(+ %1 (if (= %2 \() 1 -1)) 0 s))
+  (reduce walk-one 0 s))
+
+(defn to-basement
+  [[floor pos] i]
+  [(walk-one floor i) (inc pos)])
+
+(def not-basement (comp (partial not= -1) first))
+
+(def zero-floor [0 0])
+
+(def char-position last)
+
+(defn find-basement
+  [s]
+  (->> s
+       (reductions to-basement zero-floor)
+       (drop-while not-basement)
+       first
+       char-position))
+
--- test/count_floors/core_test.clj
+++ test/count_floors/core_test.clj
@@ -1,7 +1,7 @@
 (ns count-floors.core-test
   (:require 
     [clojure.test.check.generators :as g]
-    [clojure.test :refer [deftest is]]
+    [clojure.test :refer [deftest is testing]]
     [com.gfredericks.test.chuck.clojure-test :refer [checking]]
     [count-floors.core :refer :all]))
 
@@ -17,7 +17,25 @@
               (repeat open open-char)
               (repeat close close-char)))))
 
-(deftest count-floors
+(defn basement-gen
+  [n]
+  (g/fmap #(clojure.string/join [% close-char])
+          (g/return
+            (apply str
+                   (concat
+                     (repeat n open-char)
+                     (repeat n close-char))))))
+
+(def input-file
+  (-> "test/day1.input.txt"
+      slurp
+      clojure.string/split-lines
+      first))
+
+(deftest count-total-floors
+  (testing "Reading the input file" 
+    (is (= 138 (count-total input-file))))
+
   (checking "more open than close returns positive difference" 20
             [close g/s-pos-int 
              diff g/s-pos-int
@@ -31,7 +49,19 @@
             (is (= (- diff) (count-total coll))))
 
   (checking "same close as open returns zero" 20
-            [open g/s-pos-int 
-             coll (floor-gen open open)]
+            [open g/s-pos-int coll (floor-gen open open)]
             (is (zero? (count-total coll)))))
 
+(deftest find-basement-index
+  (testing "Walking to the basement first thing"
+    (is (= 1 (find-basement ")"))))
+
+  (testing "Walking to the basement after a few floors"
+    (is (= 5 (find-basement "()())"))))
+
+  (testing "Reading the input file" 
+    (is (= 1771 (find-basement input-file))))
+
+  (checking "The basement is the first position after same amount open and close" 40
+            [n g/s-pos-int coll (basement-gen n)]
+            (is (= (inc (* 2 n)) (find-basement coll)))))
